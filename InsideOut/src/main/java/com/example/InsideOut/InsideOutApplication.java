d9b04f51e10a8ced8b1837bb8892c63debca9545
Euna Kim <kesilver46@gmai.com>
2023-06-08 11:44:41 +0900

    initial commit

---
+++ InsideOut/src/main/java/com/example/InsideOut/InsideOutApplication.java
@@ -0,0 +1,13 @@
+package com.example.InsideOut;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsideOutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InsideOutApplication.class, args);
+	}
+
+}
