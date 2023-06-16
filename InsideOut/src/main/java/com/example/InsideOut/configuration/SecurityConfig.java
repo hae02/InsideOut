6ee92b240debbe6ab2e4493afc8290d6367d7295
qunks <sunrud6556@gmail.com>
2023-06-16 11:53:09 +0900

    SecurityConfig 파일 내용 추가

--- InsideOut/src/main/java/com/example/InsideOut/configuration/SecurityConfig.java
+++ InsideOut/src/main/java/com/example/InsideOut/configuration/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -16,6 +17,7 @@ import com.example.InsideOut.dao.UserRepository;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)
 public class SecurityConfig {
 	@Autowired
 	private UserRepository userRepository;
