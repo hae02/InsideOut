dbfe118cd697f850dabf9d1fe5898fcbbb942d5d
qunks <sunrud6556@gmail.com>
2023-06-18 21:07:57 +0900

    JWTtoken 쿠키에 저장

--- InsideOut/src/main/java/com/example/InsideOut/configuration/jwt/JwtAuthorizationFilter.java
+++ InsideOut/src/main/java/com/example/InsideOut/configuration/jwt/JwtAuthorizationFilter.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -31,19 +32,41 @@ public class JwtAuthorizationFilter extends BasicAuthenticationFilter {
 	@Override
 	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
 			throws IOException, ServletException {
-		String header = request.getHeader(JwtProperties.HEADER_STRING);
 		System.out.println("권한이나 인증이 필요한 특정 주소를 요청");
 
-
 		// header 확인
-		if (header == null || !header.startsWith(JwtProperties.TOKEN_PREFIX)) {
+//		if (header == null || !header.startsWith(JwtProperties.TOKEN_PREFIX)) {
+//			chain.doFilter(request, response);
+//			return;
+//		}
+
+		Cookie[] cookies = request.getCookies();
+
+		// 쿠키 확인
+		if (cookies == null) {
 			chain.doFilter(request, response);
 			return;
 		}
 
-		System.out.println("header : " + header);
+		String token = null;
 
-		String token = request.getHeader(JwtProperties.HEADER_STRING).replace(JwtProperties.TOKEN_PREFIX, "");
+		// 쿠키에서 JWT 토큰 추출
+		for (Cookie cookie : cookies) {
+			if (cookie.getName().equals(JwtProperties.HEADER_STRING)) {
+				token = cookie.getValue();
+				break;
+			}
+		}
+
+		// JWT 토큰 확인
+		if (token == null) {
+			chain.doFilter(request, response);
+			return;
+		}
+
+//		System.out.println("header : " + header);
+
+//		String token = request.getHeader(JwtProperties.HEADER_STRING).replace(JwtProperties.TOKEN_PREFIX, "");
 
 		String username = JWT.require(Algorithm.HMAC512(JwtProperties.SECRET)).build().verify(token)
 				.getClaim("username").asString();
@@ -52,11 +75,9 @@ public class JwtAuthorizationFilter extends BasicAuthenticationFilter {
 			MemberBean member = userRepository.findByUsername(username);
 
 			PrincipalDetails principalDetails = new PrincipalDetails(member);
-			
+
 			// 토큰이 정상이면 authentication 객체 생성
-			Authentication authentication = new UsernamePasswordAuthenticationToken(
-					principalDetails,
-					null,
+			Authentication authentication = new UsernamePasswordAuthenticationToken(principalDetails, null,
 					principalDetails.getAuthorities());
 
 			// 강제로 시큐리티의 세션에 접근하여 값 저장
--- InsideOut/src/main/java/com/example/InsideOut/configuration/jwt/JwtProperties.java
+++ InsideOut/src/main/java/com/example/InsideOut/configuration/jwt/JwtProperties.java
@@ -1,8 +1,7 @@
 package com.example.InsideOut.configuration.jwt;
 
 public interface JwtProperties {
-	String SECRET = "silver"; // 우리 서버만 알고 있는 비밀값(시크릿키)
+	String SECRET = "success"; // 우리 서버만 알고 있는 비밀값(시크릿키)
 	int EXPIRATION_TIME = 60000*20; // 20분
-	String TOKEN_PREFIX = "Bearer ";
 	String HEADER_STRING = "Authorization";
 }
